1382e4c5dd5e19f194919363e65e173ce5599ae6
Bruno Oliveira <inthemiddleh@gmail.com>
2024-07-13 11:19:49 -0300

    Classes de dominio criadas

---
+++ src/main/java/com/bruno/helpdesk/com/bruno/helpdesk/domain/Tecnico.java
@@ -0,0 +1,61 @@
+package com.bruno.helpdesk.domain;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tecnico extends Pessoa {
+
+	private List<Chamado> chamados = new ArrayList<>();
+	
+	public Tecnico() {
+		super();
+	}
+	
+	public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
+		super(id, nome, cpf, email, senha);
+	}
+
+	public Tecnico(Builder builder) {
+		super(builder.id, builder.nome, builder.cpf, builder.email, builder.senha);
+	}
+	
+	public static class Builder{
+		private Integer id;
+		private String nome;
+		private String cpf;
+		private String email;
+		private String senha;
+		
+		public Builder setId(Integer id) {
+			this.id = id;
+			return this;
+		}
+		public Builder setNome(String nome) {
+			this.nome = nome;
+			return this;
+		}
+		public Builder setCpf(String cpf) {
+			this.cpf = cpf;
+			return this;
+		}
+		public Builder setEmail(String email) {
+			this.email = email;
+			return this;
+		}
+		public Builder setSenha(String senha) {
+			this.senha = senha;
+			return this;
+		}
+		public Tecnico build() {
+			return new Tecnico(this);
+		}
+	}
+
+	public List<Chamado> getChamados() {
+		return chamados;
+	}
+
+	public void setChamados(List<Chamado> chamados) {
+		this.chamados = chamados;
+	}
+}
