212914e6f153b6077ef762683d1ae3e7e3275aff
Weells <inthemiddleh@gmail.com>
2024-07-14 15:16:39 -0300

    Perfil de teste criado

---
+++ src/main/java/com/bruno/helpdesk/services/DBService.java
@@ -0,0 +1,64 @@
+package com.bruno.helpdesk.services;
+
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Service;
+
+import com.bruno.helpdesk.domain.Chamado;
+import com.bruno.helpdesk.domain.Cliente;
+import com.bruno.helpdesk.domain.Tecnico;
+import com.bruno.helpdesk.domain.enums.Perfil;
+import com.bruno.helpdesk.domain.enums.Prioridade;
+import com.bruno.helpdesk.domain.enums.Status;
+import com.bruno.helpdesk.repositories.ChamadoRepository;
+import com.bruno.helpdesk.repositories.ClienteRepository;
+import com.bruno.helpdesk.repositories.TecnicoRepository;
+
+@Service
+public class DBService {
+
+	@Autowired
+	private TecnicoRepository tecnicoRepository;
+	
+	@Autowired
+	private ClienteRepository clienteRepository;
+	
+	@Autowired
+	private ChamadoRepository chamadoRepository;
+	
+	@Bean
+	public void instanciaDB() {
+		Tecnico tec1 = new Tecnico.Builder()
+				.setId(null)
+				.setNome("Paulo Gustavo")
+				.setCpf("11122233344")
+				.setEmail("paulo@gmail.com")
+				.setSenha("123")
+				.build();
+		tec1.addPerfil(Perfil.ADMIN);
+		
+		Cliente cli1 = new Cliente.Builder()
+				.setId(null)
+				.setNome("Linus Torvalds")
+				.setCpf("70511744013")
+				.setEmail("linus@gmail.com")
+				.setSenha("123")
+				.build();
+		
+		Chamado c1 = new Chamado.Builder()
+				.setId(null)
+				.setPrioridade(Prioridade.MEDIA)
+				.setStatus(Status.ANDAMENTO)
+				.setTitulo("Chamado 01")
+				.setObservacoes("Primeiro chamado")
+				.setTecnico(tec1)
+				.setCliente(cli1)
+				.build();
+		
+		tecnicoRepository.saveAll(Arrays.asList(tec1));
+		clienteRepository.saveAll(Arrays.asList(cli1));
+		chamadoRepository.saveAll(Arrays.asList(c1));
+	}
+}
